c28c91a9128a435a5113474252efac40b0e56ee4
hamnerlivestock <tanyahamner@yahoo.com>
2023-08-19 23:34:12 -0600

    quote

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload New one oh boy.
+          Hamner Livestock Ranch website coming soon.
         </p>
         <a
           className="App-link"
