6e015c8c8f2075f264f468aae2e41f395bedfe21
hamnerlivestock <tanyahamner@yahoo.com>
2023-08-20 18:03:52 -0600

    deleted many files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {Route, Routes, Link} from 'react-router-dom'
+// import Navbar from "./components/navigation/Navbar";
+import { AboutUs} from "./components/pages/about/AboutUs"
+import { Home } from "../src/components/pages/Home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hamner Livestock Ranch website coming soon.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+        </ul>
+      </nav>
+   
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route />
+      </Routes>
+    </>
+  )
 }
-
-export default App;
+export default App
---
+++ src/components/navigation/Navbar.js
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { Route, Routes, Link } from "react-router-dom";
+// export default function Navbar() {
+//   return (
+//     <>
+//       <nav className="nav">
+
+//     {/* <a href="/" className="site-title"> Site Name </a> */}
+//         <ul>
+//           <li className="nav-item">
+//             <Link
+//             to="/about_us"
+//             className="nav-links"
+//             >About Us
+//             </Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   </>
+//   );
+// }
