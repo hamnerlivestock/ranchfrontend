77bcb0e929ef7cbec872f6222d2bbf303541f80f
hamnerlivestock <tanyahamner@yahoo.com>
2023-08-21 08:03:40 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 
-import {Route, Routes, Link} from 'react-router-dom'
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
+// import { Redirect } from 'react-router';
+
 // import Navbar from "./components/navigation/Navbar";
 import { AboutUs} from "./components/pages/about/AboutUs"
 import { Home } from "../src/components/pages/Home"
+import Navbar from './components/navigation/Navbar'
+// import { Switch } from 'react-router-dom/cjs/react-router-dom.min'
 // import "../src/styles/App.css"
 function App() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About Us</Link>
-          </li>
-        </ul>
-      </nav>
-   
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route />
-      </Routes>
-    </>
-  )
+    
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/about_us" component={AboutUs} />
+          {/* <Redirect exact from="/home" /> */}
+        </Switch>
+      </BrowserRouter>
+       
+        {/* <Routes>
+     
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+       
+        </Routes> */}
+      
+    </div>
+  );
 }
-export default App
+export default App;
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,21 +1,30 @@
-// import React from 'react';
-// import { Route, Routes, Link } from "react-router-dom";
-// export default function Navbar() {
-//   return (
-//     <>
-//       <nav className="nav">
+import React from 'react';
+import { Link } from "react-router-dom";
+import "../../styles/navigation/navbar.css";
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
 
-//     {/* <a href="/" className="site-title"> Site Name </a> */}
-//         <ul>
-//           <li className="nav-item">
-//             <Link
-//             to="/about_us"
-//             className="nav-links"
-//             >About Us
-//             </Link>
-//         </li>
-//       </ul>
-//     </nav>
-//   </>
-//   );
-// }
+
+    {/* <a href="/" className="site-title"> Site Name </a> */}
+          <ul>
+            <li className="nav-item">
+              <Link
+              to="/about_us"
+              className="nav-links"
+              >About Us
+              </Link>
+              <Link
+              to="/home"
+              className="nav-links"
+              >Home
+              </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </>
+  );
+}
